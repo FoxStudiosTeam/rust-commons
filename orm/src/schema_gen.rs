ea2c600b6b53b50f9b137fafffebadb20b8296c0
kaiva-morphin <kaivamorphin@gmail.com>
2025-09-04 00:52:57 +0300

    transactions

--- orm/src/schema_gen.rs
+++ orm/src/schema_gen.rs
@@ -13,21 +13,18 @@ struct DBFeature {
 }
 
 
-pub const MOD_TEMPLATE : &str = include_str!("../templates/tables/mod.hbr");
-pub const DB_TABLES_TEMPLATE : &str = include_str!("../templates/db_tables.hbr");
-pub const TABLE_TEMPLATE : &str = include_str!("../templates/tables/table.hbr");
+pub const MOD_TEMPLATE : &str = include_str!("../templates/mod.hbr");
+pub const TABLE_TEMPLATE : &str = include_str!("../templates/table.hbr");
 
 pub fn generate<P: AsRef<std::path::Path>>(schema : &Schema, out_dir: P) -> Result<(), Box<dyn std::error::Error>>{
     
     let mut reg = handlebars::Handlebars::new();
 
     reg.register_template_string("mod_template", MOD_TEMPLATE)?;
-    reg.register_template_string("db_tables_template", DB_TABLES_TEMPLATE)?;
     reg.register_template_string("table_template", TABLE_TEMPLATE)?;
     reg.register_schema_reader_helpers();
 
     let tables_mod = schema.render(&reg, "mod_template")?;
-    let db_tables = schema.render(&reg, "db_tables_template")?;
 
     let dbs = &[
         DBFeature{feature: "postgres", db: "sqlx::Postgres"},
@@ -35,18 +32,12 @@ pub fn generate<P: AsRef<std::path::Path>>(schema : &Schema, out_dir: P) -> Resu
         DBFeature{feature: "sqlite", db: "sqlx::Sqlite"},
     ];
 
-    std::fs::create_dir(out_dir.as_ref().join("tables")).ok();
-
-
-    std::fs::write(out_dir.as_ref().join("tables/mod.rs"), tables_mod)?;
-    std::fs::write(out_dir.as_ref().join("db_tables.rs"), db_tables)?;
-
+    std::fs::write(out_dir.as_ref().join("mod.rs"), tables_mod)?;
     
     for (name, table) in schema.tables.iter() {
         let table = TableDBs{table, dbs};
         let rendered = reg.render("table_template", &table)?;
         std::fs::write(out_dir.as_ref()
-            .join("tables")
             .join(name)
             .with_extension("rs"), rendered)?;
     }
